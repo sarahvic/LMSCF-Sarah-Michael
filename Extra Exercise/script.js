50982234145f29e7cc17a76b2aea2cd70aa761e6
Schanom <michael.schano@gmx.at>
2020-01-23 15:02:29 +0100

    Finished Extra Exercise Exercise 1

--- Extra Exercise/script.js
+++ Extra Exercise/script.js
@@ -1,6 +1,9 @@
 //Exercise 1
 
-var a = "Hey there, i am a javascript developer, and i live in vienna";
-console.log("I" + a.substring(12,38) + ".");
-charAt
 
+var str = "Hey there, i am a javascript developer, and i live in vienna";
+var res1 = str.charAt(11)
+var res2 = str.charAt(38)
+
+
+console.log(res1.toUpperCase() + str.substring(12,38) + res2.replace(new RegExp(',', 'g'), '.'));
