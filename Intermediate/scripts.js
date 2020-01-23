2e383071cf885cab36f4e512d6497cfe90d46ce1
Schanom <michael.schano@gmx.at>
2020-01-23 10:49:41 +0100

    done Intermediate

---
+++ Intermediate/scripts.js
@@ -0,0 +1,17 @@
+
+//Exercise 1
+cars = ["Tesla","Audi","Renault","Volvo","Mazda","Fiat","Ferrari"];
+
+console.log(cars.sort());
+
+//Exercise 2
+fruits = ["Apple","Bananski","Kiwi"];
+console.log(fruits[0],fruits[1], fruits[2] + " Oragne")
+console.log(fruits[0],fruits[1], fruits[2])
+
+animals = ["Monkey", "Horse", "Dog"];
+console.log(animals.sort());
+console.log("Cat," + animals.sort());
+
+//Exercise 3
+console.log("Mango\nCherries\nKiwi\nGrapes\nPear\nPeach\nOragne\nlemon\n");
